d3d21cdcc2bcde4f71c245e61382e445350edc47
aldu-aguilar <aldu.u@hotmail.com.ar>
2020-06-10 10:16:46 -0300

    nuevos archivos

---
+++ callbacks.js
@@ -0,0 +1,11 @@
+function doble (numero){
+    return numero * 2
+};
+
+function triple (numero){
+    return numero * 3
+};
+
+function apply (numero, triple){
+    return triple(numero)
+};
---
+++ indexof.js
@@ -0,0 +1,9 @@
+let bart = {
+    madre: "Marge",
+    padre: "Homero",
+    hobbie: "Skate",
+    colorRemera: "Naranja"
+}
+for(let llave in bart){
+    console.log( bart[llave]);
+};
